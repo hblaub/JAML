91f351ee295964d5a693f2f6830bec105439500f
Harry Blauberg <harry.blauberg@googlemail.com>
2011-07-16 17:02:16 +0200

    Added license header to source files

--- jaml-jsr223/src/org/jaml/jsr223/JamlScriptEngine.java
+++ jaml-jsr223/src/org/jaml/jsr223/JamlScriptEngine.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (C) 2011 by Harry Blauberg
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package org.jaml.jsr223;
 
 import java.io.Reader;
@@ -9,8 +25,6 @@ import javax.script.ScriptContext;
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptException;
 import javax.script.SimpleBindings;
-
-import org.jaml.core.JamlReader;
 
 public class JamlScriptEngine extends AbstractScriptEngine {
 	protected ScriptEngineFactory factory;
