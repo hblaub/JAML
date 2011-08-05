8444cff77864393e8407e4a81a6004fc387cdd0f
Harry Blauberg <harry.blauberg@googlemail.com>
2011-08-05 23:30:45 +0200

    Introduction of IParserHandle and IUnit to API

---
+++ jaml-api/src/org/jaml/exceptions/ParserHandleExpectedException.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (C) 2011 by Harry Blauberg
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.jaml.exceptions;
+
+public class ParserHandleExpectedException extends RuntimeException {
+	private static final long serialVersionUID = -8003442878289508922L;
+
+	public ParserHandleExpectedException(Class<?> clazz) {
+		super(
+				String.format(
+						"Class '%s' expected to get a ParserHandle in order to operate correctly!",
+						clazz.getName()), new NullPointerException());
+	}
+}
