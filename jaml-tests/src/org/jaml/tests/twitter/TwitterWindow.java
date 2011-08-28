20cb5e71a8dd99ee994d34b34047a91db92a6cdd
Harry Blauberg <harry.blauberg@googlemail.com>
2011-08-28 05:22:23 +0200

    Twitter client is now functional, closes #3

--- jaml-tests/src/org/jaml/tests/twitter/TwitterWindow.java
+++ jaml-tests/src/org/jaml/tests/twitter/TwitterWindow.java
@@ -17,31 +17,76 @@
 package org.jaml.tests.twitter;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
-import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
 
-import org.jaml.addons.Script;
+import org.jaml.api.ICallbackEventHandler;
 import org.jaml.core.Unit;
+import org.jaml.patches.WebClient;
 
 public class TwitterWindow extends Unit<JFrame> {
-	private Collection<String> list;
+	private Collection<String> _list;
+	private JTextField _searchWord;
 
 	public TwitterWindow() {
-		list = get("list");
-		Script script = get("jScript");
-		JButton testBtn = get("testButton");
-		testBtn.addActionListener(script.compile(ActionListener.class));
+		_list = get("list");
+		_searchWord = get("teSearchWord");
 	}
 
-	public void add_button(ActionEvent event) {
-		list.add(new Date() + "");
-	}
-
-	public void del_button(ActionEvent event) {
-		list.remove(list.iterator().next());
+	public void getTweets_Click(ActionEvent event) throws URISyntaxException {
+		WebClient client = new WebClient();
+		client.downloadStringCompleted(new ICallbackEventHandler() {
+			@Override
+			public void callback(Object s, Object ea) {
+				List<String> titleList = new LinkedList<String>();
+				try {
+					// String ns = "http://www.w3.org/2005/Atom";
+					StringReader stringReader = new StringReader(ea.toString());
+					XMLStreamReader reader = XMLInputFactory.newInstance()
+							.createXMLStreamReader(stringReader);
+					while (reader.hasNext()) {
+						int event = reader.next();
+						if (event == XMLEvent.START_ELEMENT
+								&& "entry".equals(reader.getLocalName())) {
+							String title = "";
+							while (reader.hasNext()) {
+								int subEvent = reader.next();
+								if (XMLEvent.START_ELEMENT == subEvent
+										&& "title".equals(reader.getLocalName())) {
+									title = reader.getElementText();
+									break;
+								}
+							}
+							titleList.add(title);
+						}
+					}
+					_list.clear();
+					for (String title : titleList) {
+						_list.add(title);
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		if (_searchWord.getText() == null) {
+			return;
+		}
+		String searchText = _searchWord.getText().trim();
+		if (searchText.isEmpty()) {
+			return;
+		}
+		client.downloadStringAsync(new URI(String.format(
+				"http://search.twitter.com/search.atom?q=%s", searchText)));
 	}
 }
