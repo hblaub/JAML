0bb23115eee92e63cafe9fb837daca9e18f327e3
Harry Blauberg <harry.blauberg@googlemail.com>
2011-09-04 14:32:14 +0200

    API's defaults extended and commented

--- jaml-api/src/org/jaml/api/Defaults.java
+++ jaml-api/src/org/jaml/api/Defaults.java
@@ -24,17 +24,48 @@ import java.util.Locale;
  */
 public class Defaults {
 
+	/**
+	 * XML name space prefix for accessing all Java classes
+	 */
 	public static final String namespacePrefix = "http://java.com/namespaces/";
 
+	/**
+	 * XML name space for accessing special features of the JAML parser
+	 */
 	public static final String parserNamespace = "http://jaml.com/parser/2011/lang/additions";
 
+	/**
+	 * The parser's default locale if nothing else is set
+	 */
 	public static final Locale standardLocale = Locale.ENGLISH;
 
+	/**
+	 * Default file extension
+	 */
 	public static final String fileExtension = "jaml";
 
+	/**
+	 * Alternative file extension
+	 */
 	public static final String alternativeExtension = "xml";
 
+	/**
+	 * Empty string object to avoid creating always a new one
+	 */
 	public static final String emptyString = "";
 
+	/**
+	 * Name space separator for specifying the source of a Java class
+	 */
 	public static final String namespaceSourceSeparator = "?src=";
+
+	/**
+	 * MarkupLoader's begin character
+	 */
+	public static final char markupBegin = '{';
+
+	/**
+	 * MarkupLoader's end character
+	 */
+	public static final char markupEnd = '}';
 }
