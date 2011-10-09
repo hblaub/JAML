0337cbd3524d974359dad479d24bc9b30c3929c9
Harry Blauberg <harry.blauberg@googlemail.com>
2011-10-09 16:53:43 +0200

    Changed class KnownColor to enumeration

--- jaml-utils/src/org/jaml/util/KnownColor.java
+++ jaml-utils/src/org/jaml/util/KnownColor.java
@@ -18,150 +18,80 @@ package org.jaml.util;
 
 import java.awt.Color;
 
-public class KnownColor {
+/**
+ * Enumeration of all known HTML colors
+ */
+public enum KnownColor {
+	Transparent(255, 255, 255, 0), AliceBlue(240, 248, 255, 255), AntiqueWhite(
+			250, 235, 215, 255), Aqua(0, 255, 255, 255), Aquamarine(127, 255,
+			212, 255), Azure(240, 255, 255, 255), Beige(245, 245, 220, 255), Bisque(
+			255, 228, 196, 255), Black(0, 0, 0, 255), BlanchedAlmond(255, 235,
+			205, 255), Blue(0, 0, 255, 255), BlueViolet(138, 43, 226, 255), Brown(
+			165, 42, 42, 255), BurlyWood(222, 184, 135, 255), CadetBlue(95,
+			158, 160, 255), Chartreuse(127, 255, 0, 255), Chocolate(210, 105,
+			30, 255), Coral(255, 127, 80, 255), CornflowerBlue(100, 149, 237,
+			255), Cornsilk(255, 248, 220, 255), Crimson(220, 20, 60, 255), Cyan(
+			0, 255, 255, 255), DarkBlue(0, 0, 139, 255), DarkCyan(0, 139, 139,
+			255), DarkGoldenrod(184, 134, 11, 255), DarkGray(169, 169, 169, 255), DarkGreen(
+			0, 100, 0, 255), DarkKhaki(189, 183, 107, 255), DarkMagenta(139, 0,
+			139, 255), DarkOliveGreen(85, 107, 47, 255), DarkOrange(255, 140,
+			0, 255), DarkOrchid(153, 50, 204, 255), DarkRed(139, 0, 0, 255), DarkSalmon(
+			233, 150, 122, 255), DarkSeaGreen(143, 188, 139, 255), DarkSlateBlue(
+			72, 61, 139, 255), DarkSlateGray(47, 79, 79, 255), DarkTurquoise(0,
+			206, 209, 255), DarkViolet(148, 0, 211, 255), DeepPink(255, 20,
+			147, 255), DeepSkyBlue(0, 191, 255, 255), DimGray(105, 105, 105,
+			255), DodgerBlue(30, 144, 255, 255), Firebrick(178, 34, 34, 255), FloralWhite(
+			255, 250, 240, 255), ForestGreen(34, 139, 34, 255), Fuchsia(255, 0,
+			255, 255), Gainsboro(220, 220, 220, 255), GhostWhite(248, 248, 255,
+			255), Gold(255, 215, 0, 255), Goldenrod(218, 165, 32, 255), Gray(
+			128, 128, 128, 255), Green(0, 128, 0, 255), GreenYellow(173, 255,
+			47, 255), Honeydew(240, 255, 240, 255), HotPink(255, 105, 180, 255), IndianRed(
+			205, 92, 92, 255), Indigo(75, 0, 130, 255), Ivory(255, 255, 240,
+			255), Khaki(240, 230, 140, 255), Lavender(230, 230, 250, 255), LavenderBlush(
+			255, 240, 245, 255), LawnGreen(124, 252, 0, 255), LemonChiffon(255,
+			250, 205, 255), LightBlue(173, 216, 230, 255), LightCoral(240, 128,
+			128, 255), LightCyan(224, 255, 255, 255), LightGoldenrodYellow(250,
+			250, 210, 255), LightGray(211, 211, 211, 255), LightGreen(144, 238,
+			144, 255), LightPink(255, 182, 193, 255), LightSalmon(255, 160,
+			122, 255), LightSeaGreen(32, 178, 170, 255), LightSkyBlue(135, 206,
+			250, 255), LightSlateGray(119, 136, 153, 255), LightSteelBlue(176,
+			196, 222, 255), LightYellow(255, 255, 224, 255), Lime(0, 255, 0,
+			255), LimeGreen(50, 205, 50, 255), Linen(250, 240, 230, 255), Magenta(
+			255, 0, 255, 255), Maroon(128, 0, 0, 255), MediumAquamarine(102,
+			205, 170, 255), MediumBlue(0, 0, 205, 255), MediumOrchid(186, 85,
+			211, 255), MediumPurple(147, 112, 219, 255), MediumSeaGreen(60,
+			179, 113, 255), MediumSlateBlue(123, 104, 238, 255), MediumSpringGreen(
+			0, 250, 154, 255), MediumTurquoise(72, 209, 204, 255), MediumVioletRed(
+			199, 21, 133, 255), MidnightBlue(25, 25, 112, 255), MintCream(245,
+			255, 250, 255), MistyRose(255, 228, 225, 255), Moccasin(255, 228,
+			181, 255), NavajoWhite(255, 222, 173, 255), Navy(0, 0, 128, 255), OldLace(
+			253, 245, 230, 255), Olive(128, 128, 0, 255), OliveDrab(107, 142,
+			35, 255), Orange(255, 165, 0, 255), OrangeRed(255, 69, 0, 255), Orchid(
+			218, 112, 214, 255), PaleGoldenrod(238, 232, 170, 255), PaleGreen(
+			152, 251, 152, 255), PaleTurquoise(175, 238, 238, 255), PaleVioletRed(
+			219, 112, 147, 255), PapayaWhip(255, 239, 213, 255), PeachPuff(255,
+			218, 185, 255), Peru(205, 133, 63, 255), Pink(255, 192, 203, 255), Plum(
+			221, 160, 221, 255), PowderBlue(176, 224, 230, 255), Purple(128, 0,
+			128, 255), Red(255, 0, 0, 255), RosyBrown(188, 143, 143, 255), RoyalBlue(
+			65, 105, 225, 255), SaddleBrown(139, 69, 19, 255), Salmon(250, 128,
+			114, 255), SandyBrown(244, 164, 96, 255), SeaGreen(46, 139, 87, 255), SeaShell(
+			255, 245, 238, 255), Sienna(160, 82, 45, 255), Silver(192, 192,
+			192, 255), SkyBlue(135, 206, 235, 255), SlateBlue(106, 90, 205, 255), SlateGray(
+			112, 128, 144, 255), Snow(255, 250, 250, 255), SpringGreen(0, 255,
+			127, 255), SteelBlue(70, 130, 180, 255), Tan(210, 180, 140, 255), Teal(
+			0, 128, 128, 255), Thistle(216, 191, 216, 255), Tomato(255, 99, 71,
+			255), Turquoise(64, 224, 208, 255), Violet(238, 130, 238, 255), Wheat(
+			245, 222, 179, 255), White(255, 255, 255, 255), WhiteSmoke(245,
+			245, 245, 255), Yellow(255, 255, 0, 255), YellowGreen(154, 205, 50,
+			255);
 
-	private KnownColor() {
+	private Color color;
+
+	private KnownColor(int red, int green, int blue, int alpha) {
+		this.color = new Color(red, green, blue, alpha);
 	}
 
-	public static final Color Transparent = new Color(255, 255, 255, 0);
-	public static final Color AliceBlue = new Color(240, 248, 255, 255);
-	public static final Color AntiqueWhite = new Color(250, 235, 215, 255);
-	public static final Color Aqua = new Color(0, 255, 255, 255);
-	public static final Color Aquamarine = new Color(127, 255, 212, 255);
-	public static final Color Azure = new Color(240, 255, 255, 255);
-	public static final Color Beige = new Color(245, 245, 220, 255);
-	public static final Color Bisque = new Color(255, 228, 196, 255);
-	public static final Color Black = new Color(0, 0, 0, 255);
-	public static final Color BlanchedAlmond = new Color(255, 235, 205, 255);
-	public static final Color Blue = new Color(0, 0, 255, 255);
-	public static final Color BlueViolet = new Color(138, 43, 226, 255);
-	public static final Color Brown = new Color(165, 42, 42, 255);
-	public static final Color BurlyWood = new Color(222, 184, 135, 255);
-	public static final Color CadetBlue = new Color(95, 158, 160, 255);
-	public static final Color Chartreuse = new Color(127, 255, 0, 255);
-	public static final Color Chocolate = new Color(210, 105, 30, 255);
-	public static final Color Coral = new Color(255, 127, 80, 255);
-	public static final Color CornflowerBlue = new Color(100, 149, 237, 255);
-	public static final Color Cornsilk = new Color(255, 248, 220, 255);
-	public static final Color Crimson = new Color(220, 20, 60, 255);
-	public static final Color Cyan = new Color(0, 255, 255, 255);
-	public static final Color DarkBlue = new Color(0, 0, 139, 255);
-	public static final Color DarkCyan = new Color(0, 139, 139, 255);
-	public static final Color DarkGoldenrod = new Color(184, 134, 11, 255);
-	public static final Color DarkGray = new Color(169, 169, 169, 255);
-	public static final Color DarkGreen = new Color(0, 100, 0, 255);
-	public static final Color DarkKhaki = new Color(189, 183, 107, 255);
-	public static final Color DarkMagenta = new Color(139, 0, 139, 255);
-	public static final Color DarkOliveGreen = new Color(85, 107, 47, 255);
-	public static final Color DarkOrange = new Color(255, 140, 0, 255);
-	public static final Color DarkOrchid = new Color(153, 50, 204, 255);
-	public static final Color DarkRed = new Color(139, 0, 0, 255);
-	public static final Color DarkSalmon = new Color(233, 150, 122, 255);
-	public static final Color DarkSeaGreen = new Color(143, 188, 139, 255);
-	public static final Color DarkSlateBlue = new Color(72, 61, 139, 255);
-	public static final Color DarkSlateGray = new Color(47, 79, 79, 255);
-	public static final Color DarkTurquoise = new Color(0, 206, 209, 255);
-	public static final Color DarkViolet = new Color(148, 0, 211, 255);
-	public static final Color DeepPink = new Color(255, 20, 147, 255);
-	public static final Color DeepSkyBlue = new Color(0, 191, 255, 255);
-	public static final Color DimGray = new Color(105, 105, 105, 255);
-	public static final Color DodgerBlue = new Color(30, 144, 255, 255);
-	public static final Color Firebrick = new Color(178, 34, 34, 255);
-	public static final Color FloralWhite = new Color(255, 250, 240, 255);
-	public static final Color ForestGreen = new Color(34, 139, 34, 255);
-	public static final Color Fuchsia = new Color(255, 0, 255, 255);
-	public static final Color Gainsboro = new Color(220, 220, 220, 255);
-	public static final Color GhostWhite = new Color(248, 248, 255, 255);
-	public static final Color Gold = new Color(255, 215, 0, 255);
-	public static final Color Goldenrod = new Color(218, 165, 32, 255);
-	public static final Color Gray = new Color(128, 128, 128, 255);
-	public static final Color Green = new Color(0, 128, 0, 255);
-	public static final Color GreenYellow = new Color(173, 255, 47, 255);
-	public static final Color Honeydew = new Color(240, 255, 240, 255);
-	public static final Color HotPink = new Color(255, 105, 180, 255);
-	public static final Color IndianRed = new Color(205, 92, 92, 255);
-	public static final Color Indigo = new Color(75, 0, 130, 255);
-	public static final Color Ivory = new Color(255, 255, 240, 255);
-	public static final Color Khaki = new Color(240, 230, 140, 255);
-	public static final Color Lavender = new Color(230, 230, 250, 255);
-	public static final Color LavenderBlush = new Color(255, 240, 245, 255);
-	public static final Color LawnGreen = new Color(124, 252, 0, 255);
-	public static final Color LemonChiffon = new Color(255, 250, 205, 255);
-	public static final Color LightBlue = new Color(173, 216, 230, 255);
-	public static final Color LightCoral = new Color(240, 128, 128, 255);
-	public static final Color LightCyan = new Color(224, 255, 255, 255);
-	public static final Color LightGoldenrodYellow = new Color(250, 250, 210, 255);
-	public static final Color LightGray = new Color(211, 211, 211, 255);
-	public static final Color LightGreen = new Color(144, 238, 144, 255);
-	public static final Color LightPink = new Color(255, 182, 193, 255);
-	public static final Color LightSalmon = new Color(255, 160, 122, 255);
-	public static final Color LightSeaGreen = new Color(32, 178, 170, 255);
-	public static final Color LightSkyBlue = new Color(135, 206, 250, 255);
-	public static final Color LightSlateGray = new Color(119, 136, 153, 255);
-	public static final Color LightSteelBlue = new Color(176, 196, 222, 255);
-	public static final Color LightYellow = new Color(255, 255, 224, 255);
-	public static final Color Lime = new Color(0, 255, 0, 255);
-	public static final Color LimeGreen = new Color(50, 205, 50, 255);
-	public static final Color Linen = new Color(250, 240, 230, 255);
-	public static final Color Magenta = new Color(255, 0, 255, 255);
-	public static final Color Maroon = new Color(128, 0, 0, 255);
-	public static final Color MediumAquamarine = new Color(102, 205, 170, 255);
-	public static final Color MediumBlue = new Color(0, 0, 205, 255);
-	public static final Color MediumOrchid = new Color(186, 85, 211, 255);
-	public static final Color MediumPurple = new Color(147, 112, 219, 255);
-	public static final Color MediumSeaGreen = new Color(60, 179, 113, 255);
-	public static final Color MediumSlateBlue = new Color(123, 104, 238, 255);
-	public static final Color MediumSpringGreen = new Color(0, 250, 154, 255);
-	public static final Color MediumTurquoise = new Color(72, 209, 204, 255);
-	public static final Color MediumVioletRed = new Color(199, 21, 133, 255);
-	public static final Color MidnightBlue = new Color(25, 25, 112, 255);
-	public static final Color MintCream = new Color(245, 255, 250, 255);
-	public static final Color MistyRose = new Color(255, 228, 225, 255);
-	public static final Color Moccasin = new Color(255, 228, 181, 255);
-	public static final Color NavajoWhite = new Color(255, 222, 173, 255);
-	public static final Color Navy = new Color(0, 0, 128, 255);
-	public static final Color OldLace = new Color(253, 245, 230, 255);
-	public static final Color Olive = new Color(128, 128, 0, 255);
-	public static final Color OliveDrab = new Color(107, 142, 35, 255);
-	public static final Color Orange = new Color(255, 165, 0, 255);
-	public static final Color OrangeRed = new Color(255, 69, 0, 255);
-	public static final Color Orchid = new Color(218, 112, 214, 255);
-	public static final Color PaleGoldenrod = new Color(238, 232, 170, 255);
-	public static final Color PaleGreen = new Color(152, 251, 152, 255);
-	public static final Color PaleTurquoise = new Color(175, 238, 238, 255);
-	public static final Color PaleVioletRed = new Color(219, 112, 147, 255);
-	public static final Color PapayaWhip = new Color(255, 239, 213, 255);
-	public static final Color PeachPuff = new Color(255, 218, 185, 255);
-	public static final Color Peru = new Color(205, 133, 63, 255);
-	public static final Color Pink = new Color(255, 192, 203, 255);
-	public static final Color Plum = new Color(221, 160, 221, 255);
-	public static final Color PowderBlue = new Color(176, 224, 230, 255);
-	public static final Color Purple = new Color(128, 0, 128, 255);
-	public static final Color Red = new Color(255, 0, 0, 255);
-	public static final Color RosyBrown = new Color(188, 143, 143, 255);
-	public static final Color RoyalBlue = new Color(65, 105, 225, 255);
-	public static final Color SaddleBrown = new Color(139, 69, 19, 255);
-	public static final Color Salmon = new Color(250, 128, 114, 255);
-	public static final Color SandyBrown = new Color(244, 164, 96, 255);
-	public static final Color SeaGreen = new Color(46, 139, 87, 255);
-	public static final Color SeaShell = new Color(255, 245, 238, 255);
-	public static final Color Sienna = new Color(160, 82, 45, 255);
-	public static final Color Silver = new Color(192, 192, 192, 255);
-	public static final Color SkyBlue = new Color(135, 206, 235, 255);
-	public static final Color SlateBlue = new Color(106, 90, 205, 255);
-	public static final Color SlateGray = new Color(112, 128, 144, 255);
-	public static final Color Snow = new Color(255, 250, 250, 255);
-	public static final Color SpringGreen = new Color(0, 255, 127, 255);
-	public static final Color SteelBlue = new Color(70, 130, 180, 255);
-	public static final Color Tan = new Color(210, 180, 140, 255);
-	public static final Color Teal = new Color(0, 128, 128, 255);
-	public static final Color Thistle = new Color(216, 191, 216, 255);
-	public static final Color Tomato = new Color(255, 99, 71, 255);
-	public static final Color Turquoise = new Color(64, 224, 208, 255);
-	public static final Color Violet = new Color(238, 130, 238, 255);
-	public static final Color Wheat = new Color(245, 222, 179, 255);
-	public static final Color White = new Color(255, 255, 255, 255);
-	public static final Color WhiteSmoke = new Color(245, 245, 245, 255);
-	public static final Color Yellow = new Color(255, 255, 0, 255);
-	public static final Color YellowGreen = new Color(154, 205, 50, 255);
+	public Color getColor() {
+		return color;
+	}
 }
